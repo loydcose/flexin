3c383eeb1e6d34164167c35a011a27e283d39bea
Loyd Cose <loydcose.01@gmail.com>
2023-03-09 12:47:09 +0800

    Initial Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        orange: "#e76456",
+        yellow: "#f09b57",
+        "dark-full": "#000000",
+        "dark-90": "#4d4c4b",
+        "dark-85": "#585656",
+        "dark-50": "#9b9b9b",
+        "dark-40": "#afafaf",
+        "dark-10": "#ebebeb",
+      },
+    },
+  },
+  plugins: [],
+}
